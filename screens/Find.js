4a43151fc9997e70ba3371af2e5270030db54c77
Pongsakorn Srisamut <pongsakorn.srisam@ku.th>
2023-10-21 15:07:36 +0700

    Find.js add to git

---
+++ screens/Find.js
@@ -0,0 +1,19 @@
+import {StyleSheet, Text, View} from 'react-native'
+
+export const Find = () => {
+    return(
+        <View style={{flex:1}}>
+        <Text>Wow</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
+})
