05d1df7a2023886348a17152b3fe716a39a36b77
Pongsakorn Srisamut <pongsakorn.srisam@ku.th>
2023-10-22 22:33:22 +0700

    Erase expo av

--- screens/VideoPlayer.js
+++ screens/VideoPlayer.js
@@ -1,31 +1,22 @@
 import {StyleSheet, Text, View} from 'react-native'
 import {Video} from 'expo-av';
-import video from './test.mp4'
 import YoutubePlayer from 'react-native-youtube-iframe';
 
 export const VideoPlayer = () => {
     return(
         <View style={{flex:1}}>
-            {/* <Video 
-            source={video}
-            rate={1.0}
-            volume={1.0}
-            isMuted={false}
-            resizeMode="cover"
-            shouldPlay
-            isLooping
-            style={{ width: 300, height: 300 }}
-        /> */}
-        <View style={{borderColor:'black',borderWidth:1}}>
-            <YoutubePlayer
-            height={'50%'}
-            play={true}
-            videoId={'I0OUR7axHyM'}
-            width={300}
-            style={{borderColor:'red',borderWidth:1}}
-            />
-        </View>
-        <Text>Wow</Text>
+            <View style={{flex:2,height:200,borderColor:'red',borderWidth:1}}>
+            </View>
+            <View style={{flex:1,borderColor:'black',borderWidth:1}}>
+                <YoutubePlayer
+                height={'100%'}
+                play={false}
+                videoId={'I0OUR7axHyM'}
+                width={300}
+                style={{borderColor:'red',borderWidth:1}}
+                />
+            </View>
+            <Text>Wow</Text>
         </View>
     )
 }
