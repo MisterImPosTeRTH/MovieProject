a190357409f7acb003b595043bba995512d87b2f
Pongsakorn Srisamut <pongsakorn.srisam@ku.th>
2023-10-23 18:32:25 +0700

    Fix all code

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, SafeAreaView, StyleSheet, View, Platform } from 'react-native';
 import { VideoPlayer } from './screens/VideoPlayer';
-import { LoginScreen } from './Screen/Login'
-import { Decoy } from './Screen/Decoy'
-import { SignUpScreen } from './Screen/Register'
-import { RecoverScreen } from './Screen/Forget'
-import { MainScreen } from './Screen/Main'
-import { BottomTabNav } from './Screen/Navigators/bottomTabNav'
+import { LoginScreen } from './screens/Login'
+import { Decoy } from './screens/Decoy'
+import { SignUpScreen } from './screens/Register'
+import { RecoverScreen } from './screens/Forget'
+import { MainScreen } from './screens/Main'
+import { BottomTabNav } from './screens/Navigators/bottomTabNav'
 import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { StackNav } from './Screen/Navigators/stackNav'
+import { StackNav } from './screens/Navigators/stackNav'
 
 export default function App() {
   return (
